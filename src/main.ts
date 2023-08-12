086ea27f4ba923111a12e027077e74cf3c339c5d
honzaap <paneja2@seznam.cz>
2023-08-12 18:10:28 +0200

    changes in scene loading

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,31 @@ const startBtn = document.getElementById("startBtn");
 const page = document.getElementById("main");
 let isOverlay = false;
 
+let vue : typeof import("vue");
+let Scene : any;
+let vuetify : any;
+
+async function loadVue() {
+    vue = await import("vue");
+    Scene = vue.defineAsyncComponent(() =>
+        import("./pages/Scene.vue")
+    );
+    await import("vuetify/_styles.scss");
+    const vuet = await import("vuetify");
+    const components = await import("vuetify/components");
+    const directives = await import("vuetify/directives");
+    await import("@mdi/font/css/materialdesignicons.css");
+
+    vuetify = vuet.createVuetify({
+        components,
+        directives,
+    });
+}
+
 startBtn?.addEventListener("click", async function() {
     if(!page) return;
     if(!isOverlay) {
         isOverlay = true;
-        const vue = await import("vue"); //import { createApp } from "vue";
-        const Scene = vue.defineAsyncComponent(() =>
-            import("./pages/Scene.vue")
-        );
-        //await import("./pages/Scene.vue");
-
-        // Vuetify
-        await import("vuetify/_styles.scss");
-        const vuet = await import("vuetify");
-        const components = await import("vuetify/components");
-        const directives = await import("vuetify/directives");
-        await import("@mdi/font/css/materialdesignicons.css");
-
-        const vuetify = vuet.createVuetify({
-            components,
-            directives,
-        });
 
         vue.createApp(Scene).use(vuetify).provide("switchPage", () => {
             page.classList.add("overlay");
@@ -38,3 +42,4 @@ startBtn?.addEventListener("click", async function() {
 });
 
 import "./style.scss";
+loadVue();
