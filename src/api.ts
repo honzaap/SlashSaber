49256f85d5b4264910348309984fe4aedf1a22e1
honzaap <paneja2@seznam.cz>
2023-08-23 19:08:18 +0200

    fix dev api requests

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import { Settings } from "./game/models/Settings";
 const AWS_URL = "https://ivj6suabnxk5izibdy4uyp5pni0mraue.lambda-url.eu-central-1.on.aws/";
 
 export async function fetchLeaderBoard() {
+    if(import.meta.env.DEV) return [{ username: "placeholder", score: 1000 }];
     try {
         const response = await fetch(AWS_URL, {
             method: "GET"
@@ -17,6 +18,8 @@ export async function fetchLeaderBoard() {
 }
 
 export async function submitRun(settings : Settings, score : number) {
+    if(import.meta.env.DEV) return;
+        
     await fetch(AWS_URL, {
         method: "POST",
         body: JSON.stringify({
